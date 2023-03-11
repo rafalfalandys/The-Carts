41d771ab65d2242b9a67d0f2a910790bc096415a
rafalfalandys <rafalfalandys@gmail.com>
2023-03-11 23:01:28 +0100

    chart works - dots only

--- src/components/Details/Chart.js
+++ src/components/Details/Chart.js
@@ -1,9 +1,37 @@
 import { Fragment } from "react";
+import { createPriceRange } from "../../helper";
 import classes from "./Chart.module.scss";
 
 function Chart(props) {
+  const allPrices = props.cart.products.map((prod) => prod.price);
+  const maxRange = createPriceRange(allPrices)[1];
+
+  const originalPrices = props.cart.products.map((prod) => {
+    const moveValue = (20 / maxRange) * prod.price;
+    return (
+      <div
+        key={prod.id}
+        className={classes.dot}
+        style={{ transform: `translateY(-${moveValue}rem)` }}
+      ></div>
+    );
+  });
+
+  const discountedPrices = props.cart.products.map((prod) => {
+    const discountedPrice =
+      (prod.price / 100) * (100 - prod.discountPercentage);
+    const moveValue = (20 / maxRange) * discountedPrice;
+    return (
+      <div
+        key={prod.id}
+        className={`${classes.dot} ${classes["dot--discounted"]}`}
+        style={{ transform: `translateY(-${moveValue}rem)` }}
+      ></div>
+    );
+  });
+
   return (
-    <div className={classes.chart}>
+    <div className={classes.wrapper}>
       <h2 className={classes.placeholder}>
         {!props.cart && (
           <p>Click on one of the carts to display the products price chart</p>
@@ -13,8 +41,30 @@ function Chart(props) {
         <Fragment>
           <header className={classes.header}>
             <ion-icon name="cart-outline" size="large" />
-            <h2> &nbsp;Cart {props.cart.id}</h2>
+            <h2> &nbsp;Cart {props.cart.id} - Prices Chart</h2>
           </header>
+          <div className={classes.chart}>
+            <div
+              className={`${classes.line} ${classes["line-vertical"]}`}
+            ></div>
+            <div
+              className={`${classes.line} ${classes["line-horizontal"]}`}
+            ></div>
+            <h2 className={classes["price-text"]}>Price</h2>
+            <h2
+              className={`${classes["price__value"]} ${classes["price__value--min"]}`}
+            >
+              0
+            </h2>
+            <h2
+              className={`${classes["price__value"]} ${classes["price__value--max"]}`}
+            >
+              {maxRange}
+            </h2>
+
+            <div className={classes.dots}>{originalPrices}</div>
+            <div className={classes.dots}>{discountedPrices}</div>
+          </div>
         </Fragment>
       )}
     </div>
---
+++ src/helper.js
@@ -0,0 +1,15 @@
+// function generating max value of a chart. To avoid chart hitting maximum value
+export const createPriceRange = (prices) => {
+  const maxPrice = Math.max(...prices);
+  const roundFactorDigits = [...(maxPrice + "")].map((_, i) =>
+    i === 0 ? "1" : "0"
+  );
+
+  roundFactorDigits.length =
+    roundFactorDigits.length > 3 ? 3 : roundFactorDigits.length;
+
+  const roundFactor = +roundFactorDigits.join("");
+
+  const maxRange = Math.ceil((maxPrice * 1.4) / roundFactor) * roundFactor;
+  return [0, maxRange];
+};

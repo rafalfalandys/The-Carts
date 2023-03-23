d273c730b20c12a6741a6f3b40c89b0a86a0deb4
rafalfalandys <rafalfalandys@gmail.com>
2023-03-23 13:06:55 +0100

    remove old test file

--- src/tests/SingleProdForm.test.tsx
+++ src/tests/SingleProdForm.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import SingleProdForm from "../components/Form/SingleProdForm";
 import "@testing-library/jest-dom";
-import { act } from "react-dom/test-utils";
 
 test("changing price, qty, and discount affects other calculation", async () => {
   render(
@@ -12,13 +11,28 @@ test("changing price, qty, and discount affects other calculation", async () => 
     </MemoryRouter>
   );
 
-  const qtyInput = screen.getByRole("spinbutton", { name: /quantity/i });
-  const originalPriceInput = screen.getByRole("spinbutton", {
+  const qtyInput = await screen.findByRole("spinbutton", { name: /quantity/i });
+
+  const originalPriceInput = await screen.findByRole("spinbutton", {
     name: /original unit price/i,
   });
 
-  const discountInput = screen.getByRole("spinbutton", {
+  const discountInput = await screen.findByRole("spinbutton", {
     name: "Discount (%)",
+  });
+
+  act(() => {
+    userEvent.click(qtyInput);
+    userEvent.clear(qtyInput);
+    userEvent.keyboard("2");
+
+    userEvent.click(originalPriceInput);
+    userEvent.clear(originalPriceInput);
+    userEvent.keyboard("200");
+
+    userEvent.click(discountInput);
+    userEvent.clear(discountInput);
+    userEvent.keyboard("10");
   });
 
   const discUnitPrice = await screen.findByRole("textbox", {
@@ -35,18 +49,6 @@ test("changing price, qty, and discount affects other calculation", async () => 
 
   const totalDisc = await screen.findByRole("textbox", {
     name: /total discounted price/i,
-  });
-
-  act(() => {
-    userEvent.click(qtyInput);
-    userEvent.clear(qtyInput);
-    userEvent.keyboard("2");
-    userEvent.click(originalPriceInput);
-    userEvent.clear(originalPriceInput);
-    userEvent.keyboard("200");
-    userEvent.click(discountInput);
-    userEvent.clear(discountInput);
-    userEvent.keyboard("10");
   });
 
   expect(discUnitPrice).toHaveValue(`180.00`);
